b7cbc435d1c8a197942ccc5b081736e9139c1af1
Nils Brinckmann <nils.brinckmann@gfz-potsdam.de>
2019-11-07 18:30:50 +0100

    Added the exposure model for lima + update of the meta.json and prop.csv file

--- test_cmd.py
+++ test_cmd.py
@@ -62,6 +62,51 @@ class TestCmdExecution(unittest.TestCase):
 
         self.assertEqual(0, len(output))
 
+    def test_in_peru(self):
+        """
+        Runs the test with a bounding box for peru.
+        Must contain results.
+        """
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        
+        path_to_assetmaster = os.path.join(
+            current_dir,
+            'assetmaster.py'
+        )
+
+        lonmin = -84.99
+        lonmax = -67.63
+        latmin = -18.81
+        latmax = -3.33
+        schema = 'SARA_v1.0'
+        assettype = 'res'
+        querymode = 'intersects'
+
+        subprocess.run(
+            [
+                'python3',
+                path_to_assetmaster,
+                str(lonmin),
+                str(lonmax),
+                str(latmin),
+                str(latmax),
+                schema,
+                assettype,
+                querymode
+            ],
+            check=True,
+        )
+
+        path_to_output_file = os.path.join(
+            current_dir,
+            'output',
+            'query_output.geojson',
+        )
+
+        output = gpd.read_file(path_to_output_file)
+
+        self.assertLess(0, len(output))
 
     def test_in_chile(self):
         """
